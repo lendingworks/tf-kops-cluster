a6cc91a567e98e5a7cb73d2e250b0559b6d20c5a
Chin Godawita <chinthaka.godawita@lendingworks.co.uk>
2018-08-06 17:03:59 +0100

    Updated spot ASG tags.

--- module/nodes.tf
+++ module/nodes.tf
@@ -124,7 +124,13 @@ resource "aws_autoscaling_group" "node_spot" {
 
   tag = {
     key                 = "Name"
-    value               = "${var.cluster_name}-node"
+    value               = "nodes.${var.cluster_fqdn}"
+    propagate_at_launch = true
+  }
+
+  tag = {
+    key                 = "k8s.io/cluster-autoscaler/node-template/label/kops.k8s.io/instancegroup"
+    value               = "nodes"
     propagate_at_launch = true
   }
 
