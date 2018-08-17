e428f094cf636aa3bca3ad9af6d72afaeabc4293
Chin Godawita <chinthaka.godawita@lendingworks.co.uk>
2018-08-17 10:00:12 +0100

    One last go! Actually use the AMI name for the copy's name.

--- module/ami.tf
+++ module/ami.tf
@@ -1,6 +1,6 @@
 # We copy the AMI so we can (optionally) encrypt it if required.
 resource "aws_ami_copy" "k8s-ami" {
-  name              = "${local.ami_name}"
+  name              = "${data.aws_ami.k8s_ami.name}"
   description       = "An encrypted version of '${data.aws_ami.k8s_ami.name}'."
   source_ami_id     = "${data.aws_ami.k8s_ami.id}"
   source_ami_region = "${data.aws_region.current.name}"
