019163405578eeeb7da1304a79f43d4409941e8c
Chin Godawita <chinthaka.godawita@lendingworks.co.uk>
2018-08-17 09:58:06 +0100

    Use actualy AMI name in description as well.

--- module/ami.tf
+++ module/ami.tf
@@ -1,7 +1,7 @@
 # We copy the AMI so we can (optionally) encrypt it if required.
 resource "aws_ami_copy" "k8s-ami" {
   name              = "${local.ami_name}"
-  description       = "An encrypted version of '${local.ami_name}'."
+  description       = "An encrypted version of '${data.aws_ami.k8s_ami.name}'."
   source_ami_id     = "${data.aws_ami.k8s_ami.id}"
   source_ami_region = "${data.aws_region.current.name}"
   encrypted         = "${var.use_encryption}"
